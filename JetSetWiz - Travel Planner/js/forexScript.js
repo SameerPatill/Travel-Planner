45b083c046b9d6f35da3b71e81f2b11ad41ccedc
sameerpatill <sameerspatil29@gmail.com>
2024-08-21 04:15:58 +0530

    Major Changes

--- JetSetWiz - Travel Planner/js/forexScript.js
+++ JetSetWiz - Travel Planner/js/forexScript.js
@@ -45,7 +45,7 @@ function getExchangeRate(){
         amountVal = 1;
     }
     exchangeRateTxt.innerText = "Getting exchange rate...";
-    let url = `https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/${fromCurrency.value}`;
+    let url = `https://v6.exchangerate-api.com/v6/cf4cf46ac3a014bb290689d9/latest/${fromCurrency.value}`;
     fetch(url).then(response => response.json()).then(result =>{
         let exchangeRate = result.conversion_rates[toCurrency.value];
         let totalExRate = (amountVal * exchangeRate).toFixed(2);
